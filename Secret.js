fa060d4a6940d88e5ad08187b25e420ed6a08cee
Digital-Server <bramdijkstra@mijnheem.nl>
2024-07-17 22:24:23 +0200

    Update Secret.js

--- Secret.js
+++ Secret.js
@@ -1,1 +1,10 @@
-sk_live_...dqUM
+// sk_live_...dqUM
+// Minecrafgt8GB: puyGgYcrice_1PagZvIdkeXwcR0spq
+// Minecraft4GB: price_1PagXtIdkeXwcR0s4FQ9T00F
+// VPS64GB: price_1PagX1IdkeXwcR0s8YfFt3Wn
+//VPS32GB: price_1PagWJIdkeXwcR0syefeSH1U
+//VPS16GB: price_1PagVIIdkeXwcR0sWpS8DxaP
+//Minecraft16GB: price_1Pc7U6IdkeXwcR0sCQ9ghMIE
+//FXServer4GB: price_1PZWj1IdkeXwcR0s0EriqFV7
+//FXServer8GB: price_1PZXibIdkeXwcR0svX9u7ns9
+//FXServer16GB: price_1PZXs3IdkeXwcR0sfEz2b8mE
